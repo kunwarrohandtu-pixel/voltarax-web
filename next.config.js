7347cc206f361e1373d70cee2c231b6f61ae3669
Kunwar Rohan <kunwarrohan.dtu@gmail.com>
2026-08-03 03:54:33 +0530

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
   },
 }
+
 module.exports = nextConfig
